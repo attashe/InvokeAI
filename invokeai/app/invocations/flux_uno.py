cbf326ca7a96946849a6d16038a4a298916674d5
Attashe <yamihailov@gmail.com>
2025-04-16 14:23:26 -0400

    fix image load/save

--- invokeai/app/invocations/flux_uno.py
+++ invokeai/app/invocations/flux_uno.py
@@ -42,14 +42,12 @@ class FluxReduxInvocation(BaseInvocation):
     image4: Optional[ImageField] = InputField(default=None, description="4th reference")
 
     def invoke(self, context: InvocationContext) -> FluxUnoOutput:
-        images = [self.image.image_name]
 
-        if self.image2 is not None:
-            images.append(self.image2.image_name)
-        if self.image3 is not None:
-            images.append(self.image3.image_name)
-        if self.image4 is not None:
-            images.append(self.image4.image_name)
+        images: list[str] = []
+        for image in [self.image, self.image2, self.image3, self.image4]:
+            if image is not None:
+                image_pil = context.images.get_pil(self.image.image_name)
+                images.append(context.images.save(image=image_pil).image_name)
         
         return FluxUnoOutput(
             uno_refs=FluxUnoReferenceField(
